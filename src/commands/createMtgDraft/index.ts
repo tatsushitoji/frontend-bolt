b2f280f1d5034c63e10eda23825ac089d241d7c6
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-02-02 11:48:33 +0900

    fix: pascal case

--- src/commands/createMtgDraft/index.ts
+++ src/commands/createMtgDraft/index.ts
@@ -4,7 +4,7 @@ import { createDraft } from './createDraft'
 export const createMtgDraft = () => {
   app.command('/create_mtg_draft', async ({ say, ack }) => {
     ack()
-    say(`wait a minutes! Now drafting… :hammer_and_wrench:`)
+    say(`Wait a minutes! Now drafting… :hammer_and_wrench:`)
     const draftURL = await createDraft()
     say(`Here is the today's meeting draft :memo: \n ${draftURL}`)
   })
