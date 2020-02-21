dce55a010e45a7c3b614489dffa3ee9e731db7a6
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-02-21 17:58:35 +0900

    :bug: endpoints should set expressReceiver

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,13 @@ import { App, ExpressReceiver } from '@slack/bolt'
 
 export const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET || '',
-})
-
-export const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
   endpoints: {
     events: '/slack/events',
     commands: '/slack/commands',
   },
+})
+
+export const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
   receiver: expressReceiver,
 })
