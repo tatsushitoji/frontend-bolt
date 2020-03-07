e2a4be336c15fb98aa5aadd8f1532a896c85c1af
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-03-07 23:17:14 +0900

    chore: Adjust eslint (#1)
    * :wrench: Add eslint-config-airbnb eslint-plugin-import
    
    * :wrench: Adjust lint
    
    * :art: Fix lint
    
    * :wrench: Adjust condition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { app } from './app'
 import { createMtgDraft } from './commands/createMtgDraft'
-
 import { health } from './health'
 
 const runBolt = async () => {
   await app.start(process.env.PORT || 3000)
-  console.log('⚡️ Bolt app is running!')
+  console.log('⚡️ Bolt app is running!') // eslint-disable-line no-console
 }
 
 runBolt().then(() => {
